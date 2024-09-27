ded0e76e3e80eb2cd205231eee6bc51db5b64b60
NathanSinna <nathan.sinnathurai@accedo.tv>
2024-09-27 17:02:34 -0400

    move render into another module, for re-use by other controllers

--- lib/servy/bear_controller.ex
+++ lib/servy/bear_controller.ex
@@ -1,29 +1,19 @@
 defmodule Servy.BearController do
   alias Servy.Wildthings
-
-  @templates_path Path.expand("templates", File.cwd!())
-
-  defp render(conv, template, bindings \\ []) do
-    content =
-      @templates_path
-      |> Path.join(template)
-      |> EEx.eval_file(bindings)
-
-    %{conv | resp_body: content, status: 200}
-  end
+  alias Servy.View
 
   def index(conv) do
     bears =
       Wildthings.list_bears()
       |> Enum.sort_by(& &1.name)
 
-    render(conv, "index.eex", bears: bears)
+    View.render(conv, "index.eex", bears: bears)
   end
 
   def show(conv, %{"id" => id}) do
     bear = Wildthings.get_bear(id)
 
-    render(conv, "show.eex", bear: bear)
+    View.render(conv, "show.eex", bear: bear)
   end
 
   def create(conv, %{"name" => name, "type" => type}) do
